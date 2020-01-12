dbb408e70de16f53a88393e607ce959d01c882db
mirh <mirh@users.noreply.github.com>
2020-01-12 01:32:57 +0100

    Resurrected nvidia304 (w/ xorg 1.19). Added serious readme. Everything.

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 Manjaro legacy & special driver repo
 
-Because if open drivers set your PC on fire, that's better than spending some time fixing the blobs¿
+Because if open drivers set your PC on fire, that's better than spending some time fixing the blobs /s
+
+So.. Since mhwd (praise it) only supports whatever pacman supports, you'll have to add a custom repo to it. 
+But opening notepad is annoying, so here you are with a comfy command to do this (it's only needed once)
+`sudo sh -c "echo -e '[legacyhw]\nSigLevel = Optional TrustAll\nServer = file://$HOME/packages' >> /etc/pacman.conf"`
+
+Now, you can just download the repo and run one of the many build scripts there are in the root 
+(ok, truth be told I only had time to fix nvidia304xx for the moment.. the remainder will come hopefully)
+
+Once that has finished, you can just install the drivers like so:
+```
+sudo pacman -Sy mhwd-nvidia-304xx
+sudo mhwd -i pci video-nvidia-304xx
+```
--- build-nvidia-304.sh
+++ build-nvidia-304.sh
@@ -1,42 +1,33 @@
 #!/bin/bash
-rm -Rf */*nvidia*304*/{src,pkg}
-#cd linux310*/*nvidia*304*
-#makepkg -df
-#cd ../../linux312*/*nvidia*304*
-#makepkg -df
-#cd ../../linux313*/*nvidia*304*
-#makepkg -df
-#cd ../../linux314*/*nvidia*304*
-#makepkg -df
-cd linux316*/*nvidia*304*
-makepkg -df
-cd ../../linux318*/*nvidia*304*
-makepkg -df
-#cd ../../linux319*/*nvidia*304*
-#makepkg -df
-cd ../../linux41-*/*nvidia*304*
-makepkg -df
-#cd ../../linux42*/*nvidia*304*
-#makepkg -df
-#cd ../../linux43*/*nvidia*304*
-#makepkg -df
-cd ../../linux44*/*nvidia*304*
-makepkg -df
-#cd ../../linux46*/*nvidia*304*
-#makepkg -df
-#cd ../../linux47*/*nvidia*304*
-#makepkg -df
-#cd ../../linux48*/*nvidia*304*
-#makepkg -df
-cd ../../linux49*/*nvidia*304*
-makepkg -df
-#cd ../../linux410*/*nvidia*304*
-#makepkg -df
-#cd ../../linux411*/*nvidia*304*
-#makepkg -df
-cd ../../linux412*/*nvidia*304*
-makepkg -df
-cd ../../linux413*/*nvidia*304*
-makepkg -df
-cd ../../linux414*/*nvidia*304*
-makepkg -df
+
+_KERN="316 318 414 419 52 54"
+_ROOT=`dirname "$(realpath "$0")"`
+_PKG=""
+set -e
+
+rm -rf ~/packages/*.pkg.tar.xz
+mkdir -p ~/packages
+cd "${_ROOT}"/xorg119/xorg-server
+makepkg -s
+cd ../../nvidia304-server
+SRCDEST=../xorg119/xorg-server PKGDEST=~/packages makepkg -sf
+cd ../nvidia304-video
+PKGDEST=~/packages makepkg -df
+cd ../nvidia-304xx-utils
+PKGDEST=~/packages makepkg -dfC
+cd ..
+# for f in `realpath nvidia*304*/*nv*pkg.tar*`; do _PKG+=(${f}) ; done
+
+# rm -Rf */*nvidia*304*/{src,pkg}
+for k in ${_KERN[@]}; do
+  if (pacman -Q linux$k-headers > /dev/null 2>&1); then
+    cd "${_ROOT}"/linux$k*
+    PKGDEST=~/packages makepkg -df
+    #_PKG+=`realpath linux*nvidia*pkg.tar*`
+  fi
+done
+
+# echo ${_PKG[*]}
+if grep -qw legacyhw /etc/pacman.conf; then
+  repo-add ~/packages/legacyhw.db.tar.bz2 ~/packages/*.pkg.tar.xz
+fi
