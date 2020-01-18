ec3596792923ae1cd81949bb73c29c85efcfca18
mirh <mirh@users.noreply.github.com>
2020-01-18 01:02:25 +0100

    Kernels, kernels, kernels
    Thanks to http://blog.schmorp.de/2019-08-03-nvidia-legacy-304-patch-for-post-50-linux-kernels.html

--- build-nvidia-304.sh
+++ build-nvidia-304.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# TODO: somehow handle sudo's stupid passwd_timeout
-_KERN="316 318 414 419 52 54"
+# Remember sudo has stupid timeouts
+# And upstream is obnoxious (FS#65134)
+_KERN=`cat /usr/lib/modules/*/pkgbase` # or you could use&filter mhwd -li I guess?
 _ROOT=`dirname "$(realpath "$0")"`
 _PKG=""
 set -e
@@ -21,12 +22,20 @@ cd ..
 # for f in `realpath nvidia*304*/*nv*pkg.tar*`; do _PKG+=(${f}) ; done
 
 # rm -Rf */*nvidia*304*/{src,pkg}
-for k in ${_KERN[@]}; do
+for k in "316 318 414 419 54"; do
   if (pacman -Q linux$k-headers > /dev/null 2>&1); then
     cd "${_ROOT}"/linux$k-extramodules/nvidia-304xx
     SRCDEST="${_ROOT}"/nvidia-304xx-utils PKGDEST=~/packages makepkg -dfC
+    _KERN=${_KERN//linux$k/}
     #_PKG+=`realpath linux*nvidia*pkg.tar*`
+  else
+    echo "You are missing the headers for linux$k. It won't be built."
+	echo "If it's not a kernel you have installed, you shouldn't worry too much."
   fi
+done
+if [[ -z "${_KERN// }" ]]; then 
+  echo "Installed kernels $_KERN didn't have any module built." 
+  echo "It's not my business how you are going to handle that, but mhwd definitively won't like it."
 done
 
 # echo ${_PKG[*]}
@@ -34,5 +43,5 @@ if grep -qw legacyhw /etc/pacman.conf; then
   repo-add ~/packages/legacyhw.db.tar.bz2 ~/packages/*.pkg.tar.xz
 else
   echo "Don't forget to add the custom repository to your pacman.conf"
-  echo 'Then run repo-add ~/packages/legacyhw.db.tar.bz2 ~/packages/*.pkg.tar.xz'
+  echo 'Then run: repo-add ~/packages/legacyhw.db.tar.bz2 ~/packages/*.pkg.tar.xz'
 fi
