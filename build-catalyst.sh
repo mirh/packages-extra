7e593471dcbc72c0c19c6797c9175756d7f392b8
mirh <mirh@protonmail.ch>
2020-03-28 22:36:16 +0100

    Bring back catalyst

--- build-catalyst.sh
+++ build-catalyst.sh
@@ -1,22 +1,47 @@
 #!/bin/bash
-rm -Rf */*catalyst/{src,pkg}
-cd linux310*/*catalyst
-makepkg -df
-cd ../../linux312*/*catalyst
-makepkg -df
-cd ../../linux313*/*catalyst
-makepkg -df
-cd ../../linux314*/*catalyst
-makepkg -df
-cd ../../linux316*/*catalyst
-makepkg -df
-cd ../../linux318*/*catalyst
-makepkg -df
-cd ../../linux319*/*catalyst
-makepkg -df
-cd ../../linux41*/*catalyst
-makepkg -df
-cd ../../linux42*/*catalyst
-makepkg -df
-cd ../../linux43*/*catalyst
-makepkg -df
+# Remember sudo has stupid timeouts
+# And upstream is obnoxious (FS#65134)
+_KERN=`cat /usr/lib/modules/*/pkgbase`
+_ROOT=`dirname "$(realpath "$0")"`
+_PKG=""
+set -e
+
+rm -rf ~/packages/*.pkg.tar.xz
+mkdir -p ~/packages
+# cd "${_ROOT}"/xorg117/xorg-server
+# makepkg -s
+# cd ../../catalyst-server
+# SRCDEST=../xorg117/xorg-server PKGDEST=~/packages makepkg -sf
+cd "${_ROOT}"/catalyst-server
+PKGDEST=~/packages makepkg -sf
+cd ../catalyst-video
+PKGDEST=~/packages makepkg -f
+cd ../catalyst-utils
+PKGDEST=~/packages makepkg -fC
+cd ../lib32-catalyst-utils
+SRCDEST=../catalyst-utils PKGDEST=~/packages makepkg -fC
+cd ..
+
+for k in 316 318 414 419 54; do
+  if (pacman -Q linux$k-headers > /dev/null 2>&1); then
+    cd "${_ROOT}"/linux$k-extramodules/nvidia-304xx
+    SRCDEST="${_ROOT}"/nvidia-304xx-utils PKGDEST=~/packages makepkg -dfC
+    _KERN=${_KERN//linux$k/}
+    #_PKG+=`realpath linux*nvidia*pkg.tar*`
+  else
+    echo "You are missing the headers for linux$k. It won't be built."
+    echo "If it's not a kernel you have installed, you shouldn't worry too much."
+  fi
+done
+if [[ ! -z "${_KERN//$'\n'}" ]]; then
+  echo "Installed kernels $_KERN didn't have any module built."
+  echo "It's not my business how you are going to handle that, but mhwd definitively won't like it."
+fi
+
+# echo ${_PKG[*]}
+if grep -qw legacyhw /etc/pacman.conf; then
+  repo-add ~/packages/legacyhw.db.tar.bz2 ~/packages/*.pkg.tar.xz
+else
+  echo "Don't forget to add the custom repository to your pacman.conf"
+  echo 'Then run: repo-add ~/packages/legacyhw.db.tar.bz2 ~/packages/*.pkg.tar.xz'
+fi
